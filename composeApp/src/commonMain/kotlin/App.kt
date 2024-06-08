6946a5021476a36ca75f481d0f12d8d150d4147b
MaciaKing <maciasalvasalva@gmail.com>
2024-06-08 10:16:06 +0200

    [feat]: primer video finalizado https://www.youtube.com/watch?v=smAhUwEQ1d8&ab_channel=Programaci%C3%B3nAndroidbyAristiDevs

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,13 +1,19 @@
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -18,17 +24,22 @@ import helloworld.composeapp.generated.resources.compose_multiplatform
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
+        var name: String by remember { mutableStateOf("") }
+        Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+            Spacer(modifier = Modifier.height(8.dp))
+            TextField(
+                value = name,
+                onValueChange = {name = it}
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            //if(name.isNotEmpty()){
+            AnimatedVisibility(name.isNotEmpty()){
+                if(name == "macia"){
+                    Text("VAMOS !!", fontSize = 25.sp, fontWeight = FontWeight.Bold )
+                }else {
+                    Text("Hola $name", fontSize = 25.sp)
                 }
+
             }
         }
     }
